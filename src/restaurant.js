663456ecf54576c3f4dbf001bb87e15830b3f072
Vitor Sales <vitormalisa@gmail.com>
2023-10-25 18:50:06 -0300

    Projeto Concluído

--- src/restaurant.js
+++ src/restaurant.js
@@ -1,12 +1,56 @@
 /* eslint-disable max-len */
 // Siga as orientações do README!
 
-const fetchMenu = () => ({ food: {}, drinks: {} });
-// const fetchMenu = () => {
-//     return { food: {}, drinks: {} };
+;
+//  const fetchMenu = (object) => {
+//      return object;
 // };
 
-const createMenu = (obj) => ({ fetchMenu });
+const createMenu = (obj) => {
+    const newObject = { 
+        fetchMenu: () => obj,
+        consumption: [],
+        order: (product) => {
+            const foodArr = Object.keys(obj.food);
+            const drinksArr = Object.keys(obj.drinks);
+            for (let index = 0; index < foodArr.length; index += 1) {
+                if (product === foodArr[index]) {
+                    newObject.consumption.push(product);
+                    break;
+                }
+            }
+            for (let index = 0; index < drinksArr.length; index += 1) {
+                if (product === drinksArr[index]) {
+                    newObject.consumption.push(product);
+                    break;
+                }
+            }
+            return 'Item indisponível';
+        },
+        pay: () => {
+            let totalValue = 0;
+            const foodArr = Object.keys(obj.food);
+            const drinksArr = Object.keys(obj.drinks);
+            for (let index = 0; index < newObject.consumption.length; index += 1) {
+                if (foodArr.includes(newObject.consumption[index])) {
+                    totalValue += Number(obj.food[newObject.consumption[index]]);
+                }
+                if (drinksArr.includes(newObject.consumption[index])) {
+                    totalValue += Number(obj.drinks[newObject.consumption[index]]);
+                }
+            }
+            totalValue *= 1.1;
+            // return totalValue.toLocaleString('pt-br', { style: 'currency', currency: 'BRL' });
+            return totalValue.toFixed(2);
+        },
+    };
+    return newObject;
+};
+const objetoRetornadoCreateMenu = createMenu(
+    {food: {coxinha: 3.90, sanduiche: 9.90},
+    drinks: {agua: 3.90, cerveja: 6.90}});
 
-console.log(createMenu().fetchMenu());
+    objetoRetornadoCreateMenu.order('sanduiche');
+    objetoRetornadoCreateMenu.order('agua');
+    console.log(objetoRetornadoCreateMenu.pay());
 module.exports = createMenu;
--- tests/restaurant.spec.js
+++ tests/restaurant.spec.js
@@ -1,13 +1,45 @@
 const createMenu = require('../src/restaurant');
  
 describe('10 - Implemente a função `createMenu`, bem como seus casos de teste', () => {
+  const objetoRetornadoCreateMenu = createMenu(
+    {food: {coxinha: 3.90, sanduiche: 9.90},
+    drinks: {agua: 3.90, cerveja: 6.90}});
   it('Verifica se a função `createMenu` tem o comportamento esperado', () => {
     // Escreva todos os testes aqui.
-    // 1
+  });
+  it('1 - Escreva dois testes, um que verifica se a função createMenu() retorna um objeto que possui a chave fetchMenu e outro verificando se o valor da chave fetchMenu do objeto retornado pela função createMenu() é uma função.', () =>{
     expect(createMenu()).toHaveProperty('fetchMenu');
     expect(typeof createMenu().fetchMenu).toBe('function');
-    // 2
+  });
+  it('2 - Escreva um teste que verifica se o objeto retornado pela função createMenu({ food: {}, drinks: {} }).fetchMenu() retorna um objeto cujas chaves são somente food e drinks.', () => {
     expect(Object.keys(createMenu({ food: {}, drinks: {} }).fetchMenu()).length).toBe(2);
-    expect(createMenu({ food: {}, drinks: {} }).fetchMenu())
+    expect(createMenu({ food: {}, drinks: {} }).fetchMenu()).toHaveProperty('food');
+    expect(createMenu({ food: {}, drinks: {} }).fetchMenu()).toHaveProperty('drinks');
+  });
+  it('3 - Escreva um teste que verifica se o menu passado pra função createMenu() é idêntico ao menu recuperado pela função createMenu({ food: {}, drinks: {} }).fetchMenu().', () => {
+    expect({ food: {}, drinks: {} }).toEqual(createMenu({ food: {}, drinks: {} }).fetchMenu());
+  });
+  it('5 - Escreva um teste que verifica se a propriedade consumption do objeto retornado pela função createMenu({ food: {}, drinks: {} }), após a criação do menu, retorna um array vazio.', () => {
+    expect(createMenu({ food: {}, drinks: {} }).consumption.length).toBe(0);
+  });
+  it('7 - ', () => {
+    expect(objetoRetornadoCreateMenu.order('picanha')).toBe("Item indisponível");
+    objetoRetornadoCreateMenu.order('coxinha');
+    expect(objetoRetornadoCreateMenu.consumption).toContain('coxinha');
+  });
+  it('9- Escreva um teste que verifica se, ao adicionar três pedidos em sequência, dentre bebidas e comidas, o array consumption contém os itens pedidos.', () => {
+    // Já havia acrescentado uma coxinha no item anterior
+    objetoRetornadoCreateMenu.order('cerveja');
+    objetoRetornadoCreateMenu.order('agua');
+    expect(objetoRetornadoCreateMenu.consumption).toEqual(['coxinha', 'cerveja', 'agua']);
+  });
+  it('10 - Escreva um teste que verifica se a função order aceita que pedidos repetidos sejam acrescidos a consumption.', () => {
+    // Estou aproveitando os itens que já haviam sido incluidos antes
+    objetoRetornadoCreateMenu.order('agua');
+    expect(objetoRetornadoCreateMenu.consumption).toEqual(['coxinha', 'cerveja', 'agua', 'agua']);
+  });
+  it('11 - Escreva um teste que verifica que, ao chamar a função pay() que será uma propriedade do objeto retornado pela função createMenu, deve retornar a soma dos preços de tudo que foi pedido, conforme registrado em consumption. A propriedade pay tem como valor uma função.', () => {
+    expect(typeof objetoRetornadoCreateMenu.pay).toBe('function');
+    expect(objetoRetornadoCreateMenu.pay()).toBe("20.46");
   });
 });
