0619c430a7161c6797357f39b62feb31a1a00779
Vitor Sales <vitormalisa@gmail.com>
2023-10-25 13:49:26 -0300

    Lint corrigido

--- src/restaurant.js
+++ src/restaurant.js
@@ -1,6 +1,12 @@
 /* eslint-disable max-len */
 // Siga as orientações do README!
 
-const createMenu = () => {};
+const fetchMenu = () => ({ food: {}, drinks: {} });
+// const fetchMenu = () => {
+//     return { food: {}, drinks: {} };
+// };
 
+const createMenu = (obj) => ({ fetchMenu });
+
+console.log(createMenu().fetchMenu());
 module.exports = createMenu;
--- tests/restaurant.spec.js
+++ tests/restaurant.spec.js
@@ -2,7 +2,12 @@ const createMenu = require('../src/restaurant');
  
 describe('10 - Implemente a função `createMenu`, bem como seus casos de teste', () => {
   it('Verifica se a função `createMenu` tem o comportamento esperado', () => {
-    fail('Teste vazio!');
     // Escreva todos os testes aqui.
+    // 1
+    expect(createMenu()).toHaveProperty('fetchMenu');
+    expect(typeof createMenu().fetchMenu).toBe('function');
+    // 2
+    expect(Object.keys(createMenu({ food: {}, drinks: {} }).fetchMenu()).length).toBe(2);
+    expect(createMenu({ food: {}, drinks: {} }).fetchMenu())
   });
 });
